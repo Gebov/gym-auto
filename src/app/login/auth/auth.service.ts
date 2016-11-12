9e3c06a28d769591fd38b1bfbe1dba333c74c15f
gebov <martin.gebov@gmail.com>
2016-11-12 15:39:22 +0200

    integrating authentication

--- src/app/login/auth/auth.service.ts
+++ src/app/login/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
 import { IUser } from './user';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class AuthService {
+	private loginUrl = "http://localhost:5000/api/jwt";
+	constructor (private http: Http) {}
+
 	private isLoggedInField: boolean = false;
 
 	public isLoggedIn(): boolean {
@@ -11,14 +15,36 @@ export class AuthService {
 	}
 
 	public login(user: IUser): Observable<boolean> {
-		this.isLoggedInField = this.checkUser(user);
-		return Observable.of(this.isLoggedInField);
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+    	let options = new RequestOptions({ headers: headers });
+
+		return new Observable<boolean>((scrb) => {
+			this.http.post(this.loginUrl, user, options)
+                    .map((res) => res.json())
+					.subscribe((result) => {
+						this.isLoggedInField = true;
+						// handle any other claims here
+					}, (err) => {
+						this.logError(err);
+						this.isLoggedInField = false;
+					}, () => {
+						scrb.next(this.isLoggedInField);
+						scrb.complete();
+					})
+		});
 	}
 
-	private checkUser(user: IUser): boolean {
-		if (user.email === 'test@test.com' && user.password == 'test')
-			return true;
+	private logError (error: Response | any): void {
+		// In a real world app, we might use a remote logging infrastructure
+		let errMsg: string;
+		if (error instanceof Response) {
+			const statusText = error.statusText || '';
+			const body = (<any>error)._body;
+			errMsg = `${error.status} - ${statusText} ${body}`;
+		} else {
+			errMsg = error.message ? error.message : error.toString();
+		}
 
-		return false;
+		console.log(errMsg);
 	}
 }
