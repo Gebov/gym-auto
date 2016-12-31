4a28650c0a255f3766136664f594f0a4b6233961
gebov <martin.gebov@gmail.com>
2016-12-31 14:01:41 +0200

    navigation

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,35 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { AuthModel } from "./../../modules/security/state/auth.model";
 
 @Component({
 	selector: 'gym-nav',
-	templateUrl: './nav-menu.component.html'
+	templateUrl: './nav-menu.component.html',
+	styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent implements OnInit {
 	routes: Array<IRoute>;
-	constructor(private currentRoute: ActivatedRoute) {
+	constructor(private store: Store<any>) {
 	}
 
 	ngOnInit() {
-		this.routes = [
+		let routes: Array<IRoute> = [
 			{
 				title: 'Home',
 				url: '/home'
 			},
 			{
 				title: 'Administration',
-				url: '/admin'
+				url: '/admin',
+				roles: ['Administrator']
 			}
 		];
 
-		this.currentRoute.url.subscribe(x => {
-			debugger;
-		});
+		this.store.select(x => <AuthModel>x.authState).subscribe(x => {
+			let userRoles = x.data.roles;
+			let endRoutes: Array<IRoute> = [];
+
+			for (let route of routes) {
+				if (!route.roles) {
+					endRoutes.push(route);
+					continue;
+				}
+
+				for (let role of route.roles) {
+					if (userRoles.indexOf(role) !== -1) {
+						endRoutes.push(route);
+						break;
+					}
+				}
+			}
+
+			this.routes = endRoutes;
+		})
 	}
 }
 
 interface IRoute {
 	url: string,
 	title: string,
-	current?: boolean
+	roles?: Array<string>
 }
